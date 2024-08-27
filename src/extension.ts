8054caf09988a544077d739b25de8a89be407e03
mads <47996003+cl0vrfi3ld@users.noreply.github.com>
2024-08-27 17:52:43 -0400

    use channel from`upDevpod()` to provide extra context

--- src/extension.ts
+++ src/extension.ts
@@ -36,12 +36,13 @@ async function initial() {
 		return;
 	}
 
-	const reopen = { title: 'Reopen in Container' };
-	const neverAgain = { title: "Don't Show Again" };
+	const reopen = { title: 'Open Workspace in Container' };
+	// const neverAgain = { title: "Don't Show Again" };
+	const notNow = { title: "Not Right Now" };
 	const action = await vscode.window.showInformationMessage(
-		'A devcontainers file found. Reopen in a container?',
+		'A devcontainer.json file was found. Would you like to open this workspace in a container?',
 		reopen,
-		neverAgain,
+		notNow,
 	);
 	switch (action) {
 		case reopen: {
@@ -49,17 +50,24 @@ async function initial() {
 			break;
 		}
 
-		// TODO: implement 'Never Again' button
-		case neverAgain:
+		case notNow: {
 			vscode.window.showInformationMessage("Sorry, but that button is not yet implemented :)");
 			break;
+		}
+
+		// TODO: implement 'Never Again' button
+		// 	case neverAgain:
+		// 		vscode.window.showInformationMessage("Sorry, but that button is not yet implemented :)");
+		// 		break;
+		// }
 	}
 }
 
 async function openContainer(recreate: boolean = false) {
+	// the devontainer file(s) present in our workspace folder
 	const containerFiles = await vscode.workspace.findFiles(".devcontainer/**/devcontainer.json");
 	if (containerFiles.length === 0) {
-		vscode.window.showInformationMessage('No devcontainer files found.');
+		vscode.window.showErrorMessage('No devcontainer files found.');
 		return;
 	}
 	if (!devpodBinExists()) {
@@ -68,41 +76,53 @@ async function openContainer(recreate: boolean = false) {
 		return;
 	}
 
+	// the uri of the currently opened editor tab
 	const opened = vscode.window.activeTextEditor?.document?.uri;
+	// the uri of the current workspace
 	const workspace = opened ?
 		vscode.workspace.getWorkspaceFolder(opened) :
 		vscode.workspace.workspaceFolders?.at(0);
+
+	// exit if no workspace could be resolved
 	if (!workspace) {
 		vscode.window.showInformationMessage('No workspace found.');
 		return;
 	}
 
+	// i assume this is to resolve the potential existience of multiple workspace folders????
 	const picks = new Map<string, vscode.Uri>([]);
 	containerFiles.forEach(uri => {
+		// ig this gets the relative path to the workspace folder
 		const short = uri.path.replace(workspace.uri.path, '');
 		picks.set(short, uri);
 	});
 
 	let pick: vscode.Uri | undefined;
+	let pickKey: string | undefined;
 	if (picks.size === 1) {
+		pickKey = [...picks.keys()][0];
+		// grab the singular workspace folder's uri
 		pick = [...picks.values()][0];
 	}
 	if (picks.size > 1) {
+		// prompt user about which devcontainer workspace they'd like to open
 		pick = await vscode.window.showQuickPick([...picks.keys()]).then(chosen => {
 			if (!chosen) {
 				return;
 			}
+			pickKey = chosen;
 			return picks.get(chosen);
 		});
 	}
-	if (!pick) {
+	// cancel if no workspace was selected
+	if (!pick || !pickKey) {
 		return;
 	}
 
-	await upDevpod({
-		configPath: pick.path.replace(workspace.uri.path, ''),
+	let { stdoutChannel } = await upDevpod({
+		configPath: pickKey, // pick.path.replace(workspace.uri.path, ''),
 		workspaceFolder: workspace.uri.path,
-		recreate: recreate,
+		resetRecreate: recreate ? "recreate" : undefined,
 	});
 
 	const devpods = await listDevpods();
@@ -145,14 +165,16 @@ async function openContainer(recreate: boolean = false) {
 	// DevPod does not support it (yet).
 	// 
 	// TODO: show message to users or log the script's output into the output channel
+	stdoutChannel.appendLine("attempting to inject the codium remote server");
 	await installCodeServer(devpodHost, installExtArgs);
 
-	const devpodUri = vscode.Uri.from({
+
+	const devpodWorkspaceUri = vscode.Uri.from({
 		scheme: 'vscode-remote',
 		authority: `ssh-remote+${devpodHost}`,
 		path: `/workspaces/${devpod.id}`,
 	});
-	vscode.commands.executeCommand('vscode.openFolder', devpodUri);
+	vscode.commands.executeCommand('vscode.openFolder', devpodWorkspaceUri);
 }
 
 export function deactivate() { }
